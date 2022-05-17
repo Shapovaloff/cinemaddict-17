3ee8b71e9e534f02fb15c9b77b6059c9052564ed
Yury <mr.Shapovaloff3101@gmail.com>
2022-05-17 16:40:50 +0300

    1.7 Добавит презентер фильма

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import {render} from './render';
+import NavigationView from './view/navigation-view';
 import ProfileView from './view/profile-view';
+import SortView from './view/sort-view';
+import FilmsPresenter from './presenter/films-presenter';
 
 const header = document.querySelector('.header');
+const main = document.querySelector('.main');
+const filmsPresenter = new FilmsPresenter();
 
 render(new ProfileView(), header);
+
+render(new NavigationView(), main);
+render(new SortView(), main);
+
+filmsPresenter.init(main);
---
+++ src/presenter/films-presenter.js
@@ -0,0 +1,26 @@
+import {render} from '../render';
+import FilmsListView from '../view/films-list-view';
+import FilmsView from '../view/films-view';
+import FilmsListContainerView from '../view/films-list-container-view';
+import FilmCardView from './../view/film-card-view';
+import LoadMoreButtonView from '../view/load-more-button-view';
+
+export default class FilmsPresenter {
+  filmsComponent = new FilmsView();
+  filmsListComponent = new FilmsListView();
+  filmsListContainerComponent = new FilmsListContainerView();
+
+  init = (filmsContainer) => {
+    this.filmsContainer = filmsContainer;
+
+    render(this.filmsComponent, this.filmsContainer);
+    render(this.filmsListComponent, this.filmsComponent.getElement());
+    render(this.filmsListContainerComponent, this.filmsListComponent.getElement());
+
+    for (let i = 0; i < 5; i++) {
+      render(new FilmCardView(), this.filmsListContainerComponent.getElement());
+    }
+
+    render(new LoadMoreButtonView(), this.filmsListComponent.getElement());
+  };
+}
